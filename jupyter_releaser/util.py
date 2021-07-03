94bb3ed6dd22a553ad30974e708eacff9bfc51fe
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-07-03 04:21:08 -0500

    debug

--- jupyter_releaser/util.py
+++ jupyter_releaser/util.py
@@ -185,7 +185,7 @@ def bump_version(version_spec, version_cmd=""):
             version_spec == f"{version.major}.{version.minor}.{version.micro + 1}"
         else:
             version_spec == "patch"
-    print("hello 2, version_spec")
+    print("hello 2", version_spec)
 
     # Bump the version
     run(f"{version_cmd} {version_spec}")
