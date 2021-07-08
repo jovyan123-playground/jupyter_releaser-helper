93cd44d64e6e153399d458288cfafab4a77d9e85
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-07-08 09:36:34 -0500

    more debug action

--- jupyter_releaser/actions/draft_changelog.py
+++ jupyter_releaser/actions/draft_changelog.py
@@ -3,6 +3,10 @@
 from jupyter_releaser.util import run
 
 run("jupyter-releaser prep-git")
+from tbump.config import _get_config_file
+import os
+
+print(_get_config_file(os.getcwd()))
 run("jupyter-releaser bump-version")
 run("jupyter-releaser build-changelog")
 run("jupyter-releaser draft-changelog")
