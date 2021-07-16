fd143242dc2088e028ce46f3e3913abd38b351f8
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-07-16 05:49:40 -0500

    Better handling of check_release

--- jupyter_releaser/actions/draft_release.py
+++ jupyter_releaser/actions/draft_release.py
@@ -1,9 +1,29 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import os
+import shutil
+from pathlib import Path
+
+from jupyter_releaser.util import CHECKOUT_NAME
 from jupyter_releaser.util import run
+
+check_release = os.environ.get("RH_IS_CHECK_RELEASE").lower() == "true"
+
+if check_release:
+    # Extract the changelog
+    changelog_location = os.environ.get("RH_CHANGELOG", "CHANGELOG.md")
+    changelog_text = Path(changelog_location).read_text(encoding="utf-8")
+
+    # Remove the checkout
+    shutil.rmtree(CHECKOUT_NAME)
 
 run("jupyter-releaser prep-git")
 run("jupyter-releaser bump-version")
+
+if check_release:
+    # Override the changelog
+    Path(changelog_location).write_text(changelog_text)
+
 run("jupyter-releaser check-changelog")
 run("jupyter-releaser check-links")
 # Make sure npm comes before python in case it produces
