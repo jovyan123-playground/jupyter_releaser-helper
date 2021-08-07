c128255cde67e794b7b7ff69072af170cced094d
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-08-07 03:57:59 -0500

    disable pytest warnings

--- jupyter_releaser/lib.py
+++ jupyter_releaser/lib.py
@@ -49,6 +49,7 @@ def check_links(ignore_glob, ignore_links, cache_file, links_expire):
     os.makedirs(cache_dir, exist_ok=True)
     cmd = "pytest --noconftest --check-links --check-links-cache "
     cmd += f"--check-links-cache-expire-after {links_expire} "
+    cmd += "--disable-warnings "
     cmd += f"--check-links-cache-name {cache_dir}/check-release-links "
 
     ignored = []
