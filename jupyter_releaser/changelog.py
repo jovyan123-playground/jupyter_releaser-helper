c8123a64aa4473da6ed290b94bcf51e3e2e0e9f7
snuffy <41898282+github-actions[bot]@users.noreply.github.com>
2021-08-24 03:09:28 -0500

    fixups

--- jupyter_releaser/changelog.py
+++ jupyter_releaser/changelog.py
@@ -79,8 +79,9 @@ def get_version_entry(
     """
 
     if not since:
+        source = ref or branch
         tags = util.run(
-            f"git --no-pager tag --sort=-creatordate --merged {ref}", quiet=True
+            f"git --no-pager tag --sort=-creatordate --merged {source}", quiet=True
         )
         if tags:
             since = tags.splitlines()[0]
